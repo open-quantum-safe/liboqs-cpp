799f9218234b28ae346e2b9a389ebc19ae917a28
Vlad Gheorghiu <vsoftco@gmail.com>
2019-03-02 10:13:50 -0500

    updated documentation

--- doc/html/search/variables_2.js
+++ doc/html/search/variables_2.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['defined_5fname',['defined_name',['../structoqs_1_1_key_encapsulation_1_1alg__details__.html#aa984df35cf90dfc2c2f29234dd80ad41',1,'oqs::KeyEncapsulation::alg_details_::defined_name()'],['../structoqs_1_1_signature_1_1alg__details__.html#add3964b5a2e76df481f2ae342bae1a69',1,'oqs::Signature::alg_details_::defined_name()']]],
   ['details_5f',['details_',['../classoqs_1_1_key_encapsulation.html#ab14d613a959be01a065302e3ff1a9e42',1,'oqs::KeyEncapsulation::details_()'],['../classoqs_1_1_signature.html#a28b2857a9cbdf8cbd5f48153fc0711c6',1,'oqs::Signature::details_()']]]
 ];
--- doc/html/structoqs_1_1_key_encapsulation_1_1alg__details__.js
+++ doc/html/structoqs_1_1_key_encapsulation_1_1alg__details__.js
@@ -1,7 +1,6 @@
 var structoqs_1_1_key_encapsulation_1_1alg__details__ =
 [
     [ "claimed_nist_level", "structoqs_1_1_key_encapsulation_1_1alg__details__.html#a065eab79b9037d0479537d23b601d126", null ],
-    [ "defined_name", "structoqs_1_1_key_encapsulation_1_1alg__details__.html#aa984df35cf90dfc2c2f29234dd80ad41", null ],
     [ "is_ind_cca", "structoqs_1_1_key_encapsulation_1_1alg__details__.html#a3967ad4e2b7197222b0da7c2acb69e82", null ],
     [ "length_ciphertext", "structoqs_1_1_key_encapsulation_1_1alg__details__.html#a1708c06f1be664303c3e640b1a10f0d4", null ],
     [ "length_public_key", "structoqs_1_1_key_encapsulation_1_1alg__details__.html#a20b071d9dbe64dff2dc235f4b03a62ff", null ],
