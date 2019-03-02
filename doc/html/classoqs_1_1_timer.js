3809832ca5105e2d522cf2eb4224876fe156b6c8
Vlad Gheorghiu <vsoftco@gmail.com>
2019-03-02 09:10:20 -0500

    commit

---
+++ doc/html/classoqs_1_1_timer.js
@@ -0,0 +1,16 @@
+var classoqs_1_1_timer =
+[
+    [ "Timer", "classoqs_1_1_timer.html#a5184f6c79501d1b42ab0c51c4bb2e5c9", null ],
+    [ "Timer", "classoqs_1_1_timer.html#ad11a4bbd14a7ff39a939fc05f89fc756", null ],
+    [ "Timer", "classoqs_1_1_timer.html#a735ef906595568a6960c27e3bf8e98ed", null ],
+    [ "~Timer", "classoqs_1_1_timer.html#a294eb4d5cfb9227f4c19d25fddb765d4", null ],
+    [ "get_duration", "classoqs_1_1_timer.html#a4cf597874f95c86f1ef510cb32cd1e66", null ],
+    [ "operator=", "classoqs_1_1_timer.html#a00209e2f8ad28c6702d404d64aacf5eb", null ],
+    [ "operator=", "classoqs_1_1_timer.html#acb6725cfac8e1754a7f9704716ec7f91", null ],
+    [ "tic", "classoqs_1_1_timer.html#a205baf3d87e02eb3bcc2fb02f798098c", null ],
+    [ "tics", "classoqs_1_1_timer.html#a11b82d2d955e035b21dbd9f3570f02b5", null ],
+    [ "toc", "classoqs_1_1_timer.html#a2e3ce2502763032e01a5cb9ea8ea1f67", null ],
+    [ "operator<<", "classoqs_1_1_timer.html#ac925ce7106d65680a2d32ae33e6f892b", null ],
+    [ "end_", "classoqs_1_1_timer.html#a76d913ed4f0918fccf72376bf8a62aff", null ],
+    [ "start_", "classoqs_1_1_timer.html#a51fc1d3095f89c711ddecdab280b6eca", null ]
+];
---
+++ doc/html/search/classes_5.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['timer',['Timer',['../classoqs_1_1_timer.html',1,'oqs']]]
+];
