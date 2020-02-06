9a97001ac7c1a347d7a137a1fd88b29144752246
Vlad Gheorghiu <vsoftco@gmail.com>
2020-02-06 09:59:49 -0500

    switch to ds-bionic

--- unit_tests/tests/test_sig.cpp
+++ unit_tests/tests/test_sig.cpp
@@ -18,7 +18,7 @@ void test_sig(const std::string& sig_name, const oqs::bytes& msg) {
     oqs::Signature verifier{sig_name};
     bool is_valid = verifier.verify(msg, signature, signer_public_key);
     {
-//        std::lock_guard<std::mutex> lock{mut};
+        std::lock_guard<std::mutex> lock{mut};
         EXPECT_TRUE(is_valid);
         if (!is_valid)
             std::cout << sig_name << ": signature verification failed"
@@ -28,15 +28,15 @@ void test_sig(const std::string& sig_name, const oqs::bytes& msg) {
 
 TEST(oqs_Signature, Enabled) {
     oqs::bytes message = "This is our favourite message to sign"_bytes;
-//    std::vector<std::thread> thread_pool;
-//    thread_pool.reserve(oqs::Sigs::get_enabled_sigs().size());
+    std::vector<std::thread> thread_pool;
+    thread_pool.reserve(oqs::Sigs::get_enabled_sigs().size());
     for (auto&& sig_name : oqs::Sigs::get_enabled_sigs()) {
         std::cout << sig_name << std::endl;
         test_sig(sig_name, message);
-//        thread_pool.emplace_back(std::thread(test_sig, sig_name, message));
+        thread_pool.emplace_back(std::thread(test_sig, sig_name, message));
     }
-//    for (auto&& elem : thread_pool)
-//        elem.join();
+    for (auto&& elem : thread_pool)
+        elem.join();
 }
 
 TEST(oqs_Signature, NotSupported) {
