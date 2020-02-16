1fee6075e422523187c0c0aaf0142befb3063f86
Douglas Stebila <dstebila@uwaterloo.ca>
2020-02-15 20:36:10 -0500

    Build shared library

--- .travis/install_liboqs.sh
+++ .travis/install_liboqs.sh
@@ -4,6 +4,6 @@ git clone https://github.com/open-quantum-safe/liboqs
 cd liboqs
 git checkout master
 mkdir build && cd build
-cmake -GNinja ..
+cmake -GNinja -DBUILD_SHARED_LIBS=ON ..
 ninja
 ninja install
