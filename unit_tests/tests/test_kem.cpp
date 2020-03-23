905072d870062b264a8844f6abebc92f247cfe0a
Vlad Gheorghiu <vsoftco@gmail.com>
2020-03-22 23:03:39 -0400

    fixing threading issues in unit tests

--- unit_tests/tests/test_kem.cpp
+++ unit_tests/tests/test_kem.cpp
@@ -1,6 +1,7 @@
 // Unit testing oqs::KeyEncapsulation
 
 #include <iostream>
+#include <mutex>
 #include <thread>
 #include <tuple>
 #include <vector>
@@ -9,7 +10,23 @@
 
 #include "oqs_cpp.h"
 
+// no_thread_KEMs lists KEMs that have issues running in a separate thread
+static std::vector<std::string> no_thread_KEMs{
+    "Classic-McEliece-348864",  "Classic-McEliece-348864f",
+    "Classic-McEliece-460896",  "Classic-McEliece-460896f",
+    "Classic-McEliece-6688128", "Classic-McEliece-6688128f",
+    "Classic-McEliece-6960119", "Classic-McEliece-6960119f",
+    "Classic-McEliece-8192128", "Classic-McEliece-8192128f",
+    "LEDAcryptKEM-LT52"};
+
+// used for thread-safe console output
+static std::mutex mu;
+
 void test_kem(const std::string& kem_name) {
+    {
+        std::lock_guard<std::mutex> lg{mu};
+        std::cout << kem_name << std::endl;
+    }
     oqs::KeyEncapsulation client{kem_name};
     oqs::bytes client_public_key = client.generate_keypair();
     oqs::KeyEncapsulation server{kem_name};
@@ -27,17 +44,18 @@ void test_kem(const std::string& kem_name) {
 TEST(oqs_KeyEncapsulation, Enabled) {
     std::vector<std::thread> thread_pool;
     std::vector<std::string> enabled_KEMs = oqs::KEMs::get_enabled_KEMs();
-    thread_pool.reserve(enabled_KEMs.size());
     for (auto&& kem_name : enabled_KEMs) {
-        std::cout << kem_name << std::endl;
+        // use threads only for KEMs that are not in no_thread_KEMs, due to
         // issues with stack size being too small in macOS (512Kb for threads)
-        if (kem_name != "LEDAcryptKEM-LT52")
-            thread_pool.emplace_back(std::thread(test_kem, kem_name));
+        if (std::find(std::begin(no_thread_KEMs), std::end(no_thread_KEMs),
+                      kem_name) == std::end(no_thread_KEMs))
+            thread_pool.emplace_back(test_kem, kem_name);
     }
-    // test LEDAcryptKEM-LT52 in the main thread (stack size is 8Mb on macOS)
-    if (std::find(std::begin(enabled_KEMs), std::end(enabled_KEMs),
-                  "LEDAcryptKEM-LT52") != std::end(enabled_KEMs))
-        test_kem("LEDAcryptKEM-LT52");
+    // test the other KEMs in the main thread (stack size is 8Mb on macOS)
+    for (auto&& kem_name : no_thread_KEMs)
+        if (std::find(std::begin(enabled_KEMs), std::end(enabled_KEMs),
+                      kem_name) != std::end(enabled_KEMs))
+            test_kem(kem_name);
     // join the rest of the threads
     for (auto&& elem : thread_pool)
         elem.join();
--- unit_tests/tests/test_sig.cpp
+++ unit_tests/tests/test_sig.cpp
@@ -1,6 +1,7 @@
 // Unit testing oqs::Signature
 
 #include <iostream>
+#include <mutex>
 #include <thread>
 #include <vector>
 
@@ -8,7 +9,22 @@
 
 #include "oqs_cpp.h"
 
+// no_thread_sigs lists sigs that have issues running in a separate thread
+static std::vector<std::string> no_thread_sigs{"Rainbow-IIIc-Classic",
+                                               "Rainbow-IIIc-Cyclic",
+                                               "Rainbow-IIIc-Cyclic-Compressed",
+                                               "Rainbow-Vc-Classic",
+                                               "Rainbow-Vc-Cyclic",
+                                               "Rainbow-Vc-Cyclic-Compressed"};
+
+// used for thread-safe console output
+static std::mutex mu;
+
 void test_sig(const std::string& sig_name, const oqs::bytes& msg) {
+    {
+        std::lock_guard<std::mutex> lg{mu};
+        std::cout << sig_name << std::endl;
+    }
     oqs::Signature signer{sig_name};
     oqs::bytes signer_public_key = signer.generate_keypair();
     oqs::bytes signature = signer.sign(msg);
@@ -22,11 +38,20 @@ void test_sig(const std::string& sig_name, const oqs::bytes& msg) {
 TEST(oqs_Signature, Enabled) {
     oqs::bytes message = "This is our favourite message to sign"_bytes;
     std::vector<std::thread> thread_pool;
-    thread_pool.reserve(oqs::Sigs::get_enabled_sigs().size());
-    for (auto&& sig_name : oqs::Sigs::get_enabled_sigs()) {
-        std::cout << sig_name << std::endl;
-        thread_pool.emplace_back(std::thread(test_sig, sig_name, message));
+    std::vector<std::string> enabled_sigs = oqs::Sigs::get_enabled_sigs();
+    for (auto&& sig_name : enabled_sigs) {
+        // use threads only for sigs that are not in no_thread_sigs, due to
+        // issues with stack size being too small in macOS (512Kb for threads)
+        if (std::find(std::begin(no_thread_sigs), std::end(no_thread_sigs),
+                      sig_name) == std::end(no_thread_sigs))
+            thread_pool.emplace_back(test_sig, sig_name, message);
     }
+    // test the other sigs in the main thread (stack size is 8Mb on macOS)
+    for (auto&& sig_name : no_thread_sigs)
+        if (std::find(std::begin(enabled_sigs), std::end(enabled_sigs),
+                      sig_name) != std::end(enabled_sigs))
+            test_sig(sig_name, message);
+    // join the rest of the threads
     for (auto&& elem : thread_pool)
         elem.join();
 }
